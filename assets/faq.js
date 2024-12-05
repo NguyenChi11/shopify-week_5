6a5f1779dc49b7d66aaf4e4db29f0802ec90e9a4
Chí-k62 Nguyễn <chinguyen280200@gmail.com>
2024-12-05 15:05:02 +0700

    create section popup, create section blocks faq

--- assets/faq.js
+++ assets/faq.js
@@ -1,29 +1,40 @@
-const questions = document.querySelectorAll(".questions__items");
-
-questions.forEach((question) => {
-  const title = question.querySelector(".questions__title");
-  const answer = question.querySelector(".questions__answer");
-  const iconPlus = question.querySelector(".icon-plus");
-  const iconMins = question.querySelector(".icon-mins");
-
+document.querySelectorAll(".questions__title").forEach((title) => {
   title.addEventListener("click", () => {
-    // kiểm tra trạng thái
-    const isActive = answer.classList.contains("show");
-
-    // đóng tất cả các tab
-    questions.forEach((item) => {
-      item.querySelector(".questions__answer").classList.remove("show");
-      item.querySelector(".icon-plus").classList.remove("hide");
-      item.querySelector(".icon-mins").classList.add("hide");
-      item.querySelector(".questions__title").classList.remove("style");
+    const answer = title.nextElementSibling;
+    // đóng câu tl khác
+    document.querySelectorAll(".questions__answer").forEach((otherAnswer) => {
+      if (otherAnswer !== answer) {
+        otherAnswer.classList.remove("show");
+        otherAnswer.style.maxHeight = 0;
+      }
     });
 
-    // nếu tab hiên tại ko hoạt động sẽ mở
-    if (!isActive) {
-      answer.classList.add("show");
-      iconPlus.classList.add("hide");
-      iconMins.classList.remove("hide");
-      title.classList.add("style");
+    answer.classList.toggle("show");
+    // điều chỉnh chiều cao
+    if (answer.classList.contains("show")) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = 0;
     }
+
+    const icon = title.querySelector(".icon-act i");
+    if (answer.classList.contains("show")) {
+      icon.classList.remove("fa-plus");
+      icon.classList.add("fa-minus");
+      title.classList.add("show");
+    } else {
+      icon.classList.remove("fa-minus");
+      icon.classList.add("fa-plus");
+      title.classList.remove("show");
+    }
+
+    document.querySelectorAll(".questions__title").forEach((otherTitle) => {
+      if (otherTitle !== title) {
+        otherTitle.classList.remove("show");
+        const otherIcon = otherTitle.querySelector(".icon-act i");
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
   });
 });
